f638ca5a1d0badf014a1ee1fd44fc2eec2f694fd
Tee Yu June <56582497+yujune@users.noreply.github.com>
2025-06-07 08:27:25 +0800

    ui: add nav sidebar.

---
+++ app/ui/sidenav/sidenav.tsx
@@ -0,0 +1,21 @@
+import NavLinks from "@/ui/sidenav/nav-links";
+import Link from "next/link";
+
+export default function SideNav() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <Link
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        href="/"
+      >
+        <h1 className="text-2xl font-bold text-white">
+          Sales & Purchases Tracker{" "}
+        </h1>
+      </Link>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+      </div>
+    </div>
+  );
+}
