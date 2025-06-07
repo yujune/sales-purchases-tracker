7a5695a756cc86ac696c430e462ecd6702f99f3d
Tee Yu June <56582497+yujune@users.noreply.github.com>
2025-06-07 08:27:43 +0800

    chore: setup base nav structure.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import SideNav from "@/ui/sidenav/sidenav";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
