8c6cc5b1bbf3ed520ccf07e6e919e342ec28552c
Tee Yu June <56582497+yujune@users.noreply.github.com>
2025-06-09 06:12:46 +0800

    chore: add formatDate util.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -5,6 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function formatDate(date: Date | null): string {
+  if (!date) return "N/A";
+  return date.toLocaleDateString();
+}
+
 export function toDecimal(params: { value: number; decimals: number }) {
   const { value, decimals } = params;
 
