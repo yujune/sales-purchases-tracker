221c20194ed2be3ecc54e7905ed7a80bf91cc832
Tee Yu June <56582497+yujune@users.noreply.github.com>
2025-06-08 11:37:47 +0800

    chore: add toDecimal utils.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function toDecimal(params: { value: number; decimals: number }) {
+  const { value, decimals } = params;
+
+  return Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals);
 }
