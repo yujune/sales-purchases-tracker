a95233cf2ec24decd0b9fc5105a8ecacb93bdcc8
Tee Yu June <56582497+yujune@users.noreply.github.com>
2025-06-08 11:39:03 +0800

    chore: add same date transaction validation.

---
+++ app/lib/transaction-validation.ts
@@ -0,0 +1,15 @@
+import { TransactionRepository } from "../data/repo/transaction/transaction_repo";
+
+export const checkSameDateTransactionExists = async (transactionDate: Date) => {
+  const transactionRepository = new TransactionRepository();
+
+  const sameDateTransaction = await transactionRepository.getTransactionByDate(
+    transactionDate
+  );
+
+  if (!!sameDateTransaction) {
+    throw new Error(
+      "Only 1 transaction per day is allowed, please try again with a different date"
+    );
+  }
+};
