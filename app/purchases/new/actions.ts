2cb6f7c590d4a281a5281f32af4e276cd6a01cb9
Tee Yu June <56582497+yujune@users.noreply.github.com>
2025-06-08 11:39:39 +0800

    chore: add new transaction validation.

--- app/purchases/new/actions.ts
+++ app/purchases/new/actions.ts
@@ -1,9 +1,58 @@
 "use server";
 
-import { NewTransaction } from "@/app/data/database/entities";
+import { NewTransaction, Transaction } from "@/app/data/database/entities";
 import { TransactionRepository } from "@/app/data/repo/transaction/transaction_repo";
+import { checkSameDateTransactionExists } from "@/app/lib/transaction-validation";
+import { toDecimal } from "@/app/lib/utils";
 
-export async function createPurchase(transaction: NewTransaction) {
+export interface NewPurchaseTransaction {
+  quantity: number;
+  unitPrice: number;
+  date: Date;
+}
+
+function calculateNewWac(
+  latestPurchase: Transaction,
+  newPurchase: NewPurchaseTransaction
+) {
+  const totalInventoryValue =
+    latestPurchase.totalInventoryQuantity * latestPurchase.wac;
+
+  const totalNewPurchaseValue = newPurchase.unitPrice * newPurchase.quantity;
+
+  const newTotalInventoryQuantity =
+    latestPurchase.totalInventoryQuantity + newPurchase.quantity;
+
+  return (
+    (totalInventoryValue + totalNewPurchaseValue) / newTotalInventoryQuantity
+  );
+}
+
+export async function createPurchase(transaction: NewPurchaseTransaction) {
+  await checkSameDateTransactionExists(transaction.date);
+
   const transactionRepository = new TransactionRepository();
-  return await transactionRepository.create(transaction);
+  const latestPurchase = await transactionRepository.getLatestTransaction(
+    "PURCHASE"
+  );
+
+  let wac = transaction.unitPrice;
+  let totalInventoryQuantity = transaction.quantity;
+
+  if (!!latestPurchase) {
+    wac = calculateNewWac(latestPurchase, transaction);
+    totalInventoryQuantity =
+      latestPurchase.totalInventoryQuantity + transaction.quantity;
+  }
+
+  const newTransaction: NewTransaction = {
+    quantity: transaction.quantity,
+    unitPrice: toDecimal({ value: transaction.unitPrice, decimals: 2 }),
+    type: "PURCHASE",
+    wac: toDecimal({ value: wac, decimals: 2 }),
+    totalInventoryQuantity: totalInventoryQuantity,
+    createdAt: transaction.date,
+  };
+
+  return await transactionRepository.create(newTransaction);
 }
--- app/sales/new/actions.ts
+++ app/sales/new/actions.ts
@@ -2,8 +2,44 @@
 
 import { NewTransaction } from "@/app/data/database/entities";
 import { TransactionRepository } from "@/app/data/repo/transaction/transaction_repo";
+import { checkSameDateTransactionExists } from "@/app/lib/transaction-validation";
+import { toDecimal } from "@/app/lib/utils";
 
-export async function createSale(transaction: NewTransaction) {
+export interface NewSaleTransaction {
+  quantity: number;
+  unitPrice: number;
+  date: Date;
+}
+
+export async function createSale(transaction: NewSaleTransaction) {
+  await checkSameDateTransactionExists(transaction.date);
+
   const transactionRepository = new TransactionRepository();
-  return await transactionRepository.create(transaction);
+  const latestTransaction = await transactionRepository.getLatestTransaction();
+
+  if (!latestTransaction) {
+    throw new Error("No inventory found, please add a purchase first");
+  }
+
+  const latestTotalInventoryQuantity = latestTransaction.totalInventoryQuantity;
+  const saleQuantity = transaction.quantity;
+
+  if (latestTotalInventoryQuantity < saleQuantity) {
+    const missingQuantity = saleQuantity - latestTotalInventoryQuantity;
+    throw new Error(
+      `Not enough inventory, please add ${missingQuantity} more items`
+    );
+  }
+
+  const newTotalInventoryQuantity = latestTotalInventoryQuantity - saleQuantity;
+
+  const newTransaction: NewTransaction = {
+    quantity: saleQuantity,
+    unitPrice: toDecimal({ value: transaction.unitPrice, decimals: 2 }),
+    type: "SALE",
+    wac: toDecimal({ value: latestTransaction.wac, decimals: 2 }),
+    totalInventoryQuantity: newTotalInventoryQuantity,
+  };
+
+  return await transactionRepository.create(newTransaction);
 }
